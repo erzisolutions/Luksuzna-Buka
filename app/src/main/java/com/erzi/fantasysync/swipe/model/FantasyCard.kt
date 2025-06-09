36737ee57292be3618d6cf46c1547fd868996b3a
ERZi <erzi.solutions@gmail.com>
2025-06-09 05:33:26 +0200

    Add FantasySync swipe deck feature

---
+++ app/src/main/java/com/erzi/fantasysync/swipe/model/FantasyCard.kt
@@ -0,0 +1,10 @@
+package com.erzi.fantasysync.swipe.model
+
+/**
+ * Represents a single item displayed in the swipe deck.
+ */
+data class FantasyCard(
+    val id: String,
+    val title: String,
+    val description: String
+)
